038c6834a1a6ccc447ff74f72622b8c540210972
new-hero <tccdcc8@gmail.com>
2024-05-27 20:22:10 +0600

    dashboard layout modufy

--- client/src/Layouts/UserLayout.jsx
+++ client/src/Layouts/UserLayout.jsx
@@ -6,68 +6,24 @@ const UserLayout = () => {
       <NavLink className="btn capitalize m-1" to="/">
         Home
       </NavLink>
-      <NavLink className="btn capitalize m-1" to="about">
-        about
-      </NavLink>
-      <NavLink className="btn capitalize m-1" to="contact">
-        contact
-      </NavLink>
-      <NavLink className="btn capitalize m-1" to="blogs">
-        Blogs
-      </NavLink>
       <NavLink className="btn capitalize m-1" to="">
-        dashboard
+        Dashboard
+      </NavLink>
+      <NavLink className="btn capitalize m-1" to="manageProducts">
+        Products
+      </NavLink>
+      <NavLink className="btn capitalize m-1" to="addProduct">
+        Add Product
       </NavLink>
     </>
   );
   return (
-    <div className="drawer max-w-5xl m-auto">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
-        {/* Navbar */}
-        <div className="w-full navbar bg-base-300">
-          <div className="flex-none lg:hidden">
-            <label
-              htmlFor="my-drawer-3"
-              aria-label="open sidebar"
-              className="btn btn-square btn-ghost"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-6 h-6 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </label>
-          </div>
-          <div className="flex-1 px-2 mx-2">Dashboard</div>
-          <div className="flex-none hidden lg:block">
-            <ul className="menu menu-horizontal">
-              {/* Navbar menu content here */}
-              {/* {menu} */}
-            </ul>
-          </div>
-        </div>
-        {/* Page content here */}
-        <Outlet />
+    <div className="flex ">
+      <div className="w-2/12 bg-slate-300 m-2 h-screen">
+
       </div>
-      <div className="drawer-side">
-        <label
-          htmlFor="my-drawer-3"
-          aria-label="close sidebar"
-          className="drawer-overlay"
-        ></label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200">
-          {/* Sidebar content here */}
-          {/* {menu} */}
-        </ul>
+      <div className="w-10/12">
+
       </div>
     </div>
   );
